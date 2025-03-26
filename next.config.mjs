29054719f14335e32eba6d4e44ecd4aadeeee0e8
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-03-26 22:04:45 +0700

    Feat: Refactor some code

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,17 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
